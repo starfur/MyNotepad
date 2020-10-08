d297fd9428a13c437220dc1bb3c0e46d67a2e031
furkan <furkan.comert>
2020-10-08 08:10:50 +0300

    add button listener is added

--- app/src/main/java/com/example/mynotepad/MainActivity.java
+++ app/src/main/java/com/example/mynotepad/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.mynotepad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView myRecyclerView;
+    FloatingActionButton addButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        addButton= (FloatingActionButton) findViewById(R.id.addB);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
