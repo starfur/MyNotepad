70e6a68229aef8d2010350803620dffa20522aea
furkan <furkan.comert@arcelik.com>
2020-10-17 11:12:38 +0300

    New xml is added

--- app/src/main/java/com/example/mynotepad/MainActivity.java
+++ app/src/main/java/com/example/mynotepad/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.mynotepad;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -22,8 +30,28 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showEnterNote();
             }
         });
     }
+
+
+
+    public void showEnterNote(){
+        final EditText title, des;
+        Button submit;
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        dialog.setContentView(R.layout.enternote);
+        params.copyFrom(dialog.getWindow().getAttributes());
+        params.height = WindowManager.LayoutParams.MATCH_PARENT;
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.gravity = Gravity.CENTER;
+        dialog.getWindow().setAttributes(params);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+
 }
